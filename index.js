c182c49d00700fce4f5d598d71dc11483adb7e3f
suryanshsoni27 <szscode2727@gmail.com>
2022-03-22 00:23:41 -0500

    update rtge

--- index.js
+++ index.js
@@ -1,47 +1,88 @@
 const repl = require("repl");
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
 
-global.datastore = {
-    warehouses:{
-        warehouse:{
-            products:[],
-            stock:0,
-            sku:null,
-        }
-    }
+var warehouses = {
 }
 
-function addProduct(uInput) {   
-    datastore.warehouses.warehouse.product.push(uInput)
+var catlog = {
 
+}
+function makeNameForWarehouse() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 5; i++)
+    text += possible. charAt(Math. floor(Math. random() * possible. length));
+    return text;
+}
+
+function addProduct(productName,sku) {   
+    if(sku in catlog){
+        console.log(`ERROR EXISTING PRODUCT, PRODUCT WITH SKU ${sku} ALREADY EXISTS`)
+        return 
+    }
+    else{
+    catlog[sku.replace(' ','')] = productName
+    }
     
 }
-function addWarehouse(uInput,datastore) {
+function addWarehouse(uInput=Number.MAX_SAFE_INTEGER,warehouses) {
+    if(uInput in warehouses){
+        console.log(`ERROR ADDING WAREHOUSE, WAREHOUSE WITH NUMBER ${uInput} ALREADY EXISTS`)
+        return 
+    }
+    else{
+    const name = makeNameForWarehouse()
+    let stockvalue = parseInt(uInput);
+    let newwarehouse = {warehousename:name,warehousenumber:stockvalue,stock:stockvalue,product:{}}
+    warehouses[stockvalue]=(newwarehouse)
+    }
 
 }
-function stock(uInput,datastore) {
-
-}
-function unstock(uInput,datastore) {
-
-}
-function listProducts(uInput,datastore) {
-
-}
-function listWarehouse(uInput,datastore) {
-    console.log(global.datastoredatastore.warehouses.warehouse[0].product);
-
+function stock(sku,warehousenumber,quantity) {
+    let productTobeAdded = {}
+    productTobeAdded['sku'] = sku;
+    productTobeAdded['name'] = catlog[sku]
+    productTobeAdded['quantity'] = parseInt(quantity);
+    warehouses[warehousenumber]['product'][sku] = productTobeAdded
 }
 
-function listWarehouses(uInput,datastore) {
+function unstock(sku,warehousesnumber,quantityTobeRemoved) {
+    let currentQuantity = warehouses[warehousesnumber]['product'][sku]['quantity']
+    const updatedQuantity = (currentQuantity - parseInt(quantityTobeRemoved)) > 0?currentQuantity - parseInt(quantityTobeRemoved):0;
+    let updatedProductTobeAdded = {}
+    updatedProductTobeAdded[sku] = sku
+    updatedProductTobeAdded['name'] = catlog[sku]
+    updatedProductTobeAdded['quantity'] = updatedQuantity
+    warehouses[warehousenumber]['product'][sku] = updatedProductTobeAdded
+}
 
+function listProducts() {
+    let skus = Object.keys(catlog)
+    skus.forEach((sku)=>{
+        console.log(catlog[sku] + " " + sku)
+    })
+    
+}
+function listWarehouses() {
+    let warehouse = Object.keys(warehouses)
+    console.log("WAREHOUSES")
+    warehouse.forEach((k)=>console.log(k))
+    
+}
+
+function listWarehouse(warehousenumber) {
+    var obj = warehouses[parseInt(warehousenumber)]['product'];
+    console.log(warehouses[parseInt(warehousenumber)])
+    console.log("ITEM NAME              ITEM_SKU")
+    Object.keys(obj).forEach(function(key) {
+        console.log(obj[key].name + "              "+key)
+        console.log(obj[key].quantity)
+    });
+    
+    // warehouses[parseInt(warehousenumber)]['product'].forEach((k)=>{
+    //     console.log(k.name + "              "+k.sku)
+    //     console.log(k.quantity)
+    // })
 }
 
 
@@ -49,24 +90,39 @@ function listWarehouses(uInput,datastore) {
 
 function checkFunction(uInput,datastore) {
     const request = uInput.split(" ")
-    console.log(request)
     if((request[0] + request[1]) == "ADDWAREHOUSE") {
-        addWarehouse(request[2]);
-        console.log(request[2])
+        addWarehouse(request[2],warehouses);
     }
-    else if(request[0] == "LIST" && request[1] == "WAREHOUSE\n") {
-        console.log("I AM HERE NOW",request)
-        console.log(datastore)
+    if((request[0] + request[1]) == "ADDPRODUCT") {
+        const nameOfProduct = uInput
+            .match(/(?:"[^"]*"|^[^"]*$)/)[0]
+            .replace(/"/g, "")
+        let dataExtracted = uInput.split('"')
+        const sku = dataExtracted[2].replace(/\n/g, '')
+        addProduct(nameOfProduct,sku);
+    }
+    else if(request[0] == "LIST" && request[1].replace(/\n/g, '') == "WAREHOUSES") {
+        listWarehouses()
+    }
+    else if(request[0] == "LIST" && request[1].replace(/\n/g, '') == "WAREHOUSE") {
+        listWarehouse(request[2])
+    }
+    else if(request[0] == "LIST" && request[1].replace(/\n/g, '') == "PRODUCTS") {
+        listProducts()
+    }
+
+    else if(request[0] == "STOCK") {
+        stock(request[1],request[2],request[3])
+    }
+    else if(request[0] == "UNSTOCK") {
+        console.log(request)
+        unstock(request[1],request[2],request[3])
     }
 
 
 }
 
 
-repl.start({ prompt: "custom-repl => ", eval: checkFunction});
+repl.start({ prompt: "", eval: checkFunction});
 repl
 
-
-// function modifyOutput(output) {
-//     return output.toUpperCase().red;
-//   }
